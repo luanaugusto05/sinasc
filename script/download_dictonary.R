3245cc826cb3de4fcab5baa1bd1c4d8da1a43f0b
Luan Augusto <luanguto87@gmail.com>
2024-10-02 16:52:09 -0300

    Script to download the dictionaries

---
+++ script/download_dictonary.R
@@ -0,0 +1,28 @@
+#$______________________________________________________________________________
+#Importing the librarys
+
+library(curl)
+library(rvest)
+library(stringr)
+library(foreign)
+library(dplyr)
+
+#$______________________________________________________________________________
+
+dic <- c("CADMUN","CID10","TABUF")
+
+for (i in dic) {
+  temp <- tempfile(fileext = ".DBF")
+  download.file(paste0("ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/1996_/TABELAS/",i,".DBF"),
+                destfile = temp, mode = "wb", method = "libcurl")
+  data <- read.dbf(temp)
+  assign(i,data)
+}
+
+temp <- tempfile(fileext = ".zip")
+download.file("https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/cbo/servicos/downloads/estrutura-cbo.zip",destfile = temp)
+temp_dir <- tempdir()
+unzip(temp,files = c("CBO2002 - Ocupacao.csv"),exdir = temp_dir)
+ocup <- read.csv2(paste0(temp_dir,"\\CBO2002 - Ocupacao.csv"))
+
+usethis::use_data(CADMUN,CID10,TABUF,ocup, overwrite = T)
