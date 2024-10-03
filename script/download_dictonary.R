e2fea5f45e5ccad199b68643c4d5e69781c35602
Luan Augusto <luanguto87@gmail.com>
2024-10-03 14:58:45 -0300

    Update script.

--- script/download_dictonary.R
+++ script/download_dictonary.R
@@ -6,6 +6,7 @@ library(rvest)
 library(stringr)
 library(foreign)
 library(dplyr)
+library(zip)
 
 #$______________________________________________________________________________
 
@@ -24,5 +25,6 @@ download.file("https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/cbo/servicos
 temp_dir <- tempdir()
 unzip(temp,files = c("CBO2002 - Ocupacao.csv"),exdir = temp_dir)
 ocup <- read.csv2(paste0(temp_dir,"\\CBO2002 - Ocupacao.csv"))
+ocup$CODIGO <- as.factor(ocup$CODIGO)
 
 usethis::use_data(CADMUN,CID10,TABUF,ocup, overwrite = T)
